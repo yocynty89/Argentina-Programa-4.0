0e23e0102d38996031bd3a00a97067124b07f2de
Cintia Martinez <cintialujan@hotmail.com>
2023-03-04 13:36:53 -0300

    Falta terminar de desarrollar ej 2

---
+++ clase3Ejercicio2.java
@@ -0,0 +1,34 @@
+public class clase3Ejercicio2 {
+    public static void main(String[] args) {
+    //Genere una clase que tenga los métodos para realizar la codificación y decodificación
+    //de un string, dado un número de desplazamiento.
+                String palabra="hola que tal";
+                int desplazamineto=1;
+                String codificado="";
+                int cantCaracteres=palabra.length();
+                String abecedario="abcdefghijklmnñopqrstuvwxyz";
+                char letra,letraAbc;
+                int cantCaracteresAbc=abecedario.length();
+
+
+
+                for( int i = 0;
+                     i<cantCaracteres;i++){
+                    letra=palabra.charAt(i);//accede a cada uno de las letras
+
+
+                    for (int x = 0; x < cantCaracteresAbc; x++) {
+
+                        letraAbc=abecedario.charAt(x) ;
+
+                        if(letra==letraAbc){
+                            letraAbc=abecedario.charAt(x+desplazamineto);
+                            codificado= codificado+ codificado.valueOf(letraAbc);
+                        }
+                    }
+
+                }
+                System.out.println(codificado);
+            }}
+
+
